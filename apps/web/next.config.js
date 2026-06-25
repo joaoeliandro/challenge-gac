0d19290526936459c6a79516ed688482d549a31d
joaoeliandro <eliandrogermano@gmail.com>
2026-06-25 16:49:56 -0300

    build: config next setup

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
   env: {
     API_URL: process.env.API_URL || 'http://localhost:3001',
   },
